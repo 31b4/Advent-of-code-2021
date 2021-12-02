dad36035fbc3c7dc3675cb50a5ede1f063b453a6
31b4 <75566095+31b4@users.noreply.github.com>
2021-12-02 06:31:41 +0100

    Add files via upload

---
+++ day2/main.py
@@ -0,0 +1,33 @@
+from os import spawnl
+
+
+with open('input.txt') as f:
+    lines = f.readlines()
+horizontal =0
+depth = 0
+for line in lines:
+    word = line.split(' ')
+
+
+    if word[0]== "forward":
+        horizontal+=int(word[1])
+    elif word[0] == "down":
+        depth +=int(word[1])
+    elif word[0] == "up":
+        depth-=int(word[1])
+
+print(horizontal*depth)
+horizontal = 0
+depth=0
+aim = 0
+for line in lines:
+    word = line.split(' ')
+    if word[0]== "forward":
+        horizontal+=int(word[1])
+        depth += aim*int(word[1])
+    elif word[0] == "down":
+        aim +=int(word[1])
+    elif word[0] == "up":
+        aim -=int(word[1])
+print(horizontal*depth)
+
