6e5a22f5bc2cc6f4b9977381b78b8692b50ffb0a
31b4 <75566095+31b4@users.noreply.github.com>
2021-12-06 07:15:38 +0100

    python

---
+++ day6/main.py
@@ -0,0 +1,19 @@
+def Read_all():
+    with open("input.txt") as f:
+        nums=  f.readline().split(',')
+    return [int(i) for i in nums]
+
+def Part1(line):
+    for i in range(80):
+        for x in range(len(line)):
+            if line[x]==0:
+                line[x]=6
+                line.append(8)
+            else:
+                line[x] -=1
+    return line
+def Part2(nums):
+    pass
+nums = Read_all()
+print("part 1: ",len(Part1(nums)))
+
