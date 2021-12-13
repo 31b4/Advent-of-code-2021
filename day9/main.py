7f1e64cf8f739714295a324e43af6371a820c3c3
31b4 <75566095+31b4@users.noreply.github.com>
2021-12-13 13:01:00 +0100

    Add files via upload

--- day9/main.py
+++ day9/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 def Read_all():
     with open("input.txt") as f:
         return f.read().split('\n')
@@ -25,24 +24,38 @@ def Part1(lines):
                 if int(lines[i + 1][j]) < current:
                     notSafe = True
             if not notSafe:
-                print(current)
                 summ += 1 + int(current)
                 lows.append((i,j))
     return lows, summ
 def Part2(lines,lows):
-    current_id = 0
-    for row,col in lows: #Depth-first search
-        current = [(row,col)]
-        visited = set()
-        while len(current) > 0:
-            row, col = current.pop()
-            if (row,col) in visited:
-                continue
-            visited.add((row,col))
-            ids[row,col]= current_id
+    pools = []
+    for x,y in lows:
+        opens = [[x,y]]
+        closes = []
+        while len(opens) > 0:
+            cX = opens[0][0]
+            
+            cY = opens[0][1] #jelenlegi pos
+            closes.append(opens[0])
+            del opens[0]
+            for m in [[0,1],[1,0],[0,-1],[-1,0]]:
+                if not cX+m[1] == -1 and not cY+m[0] == -1:
+                    try: 
+                            if not [cX+m[1],cY+m[0]] in opens and not [cX+m[1],cY+m[0]] in closes and  not int(lines[cX+m[1]][cY+m[0]])==  9:
+                                opens.append([cX+m[1],cY+m[0]])
+                    except IndexError:
+                        a = 0
+        pools.append(len(closes))
+    
+
+
+    pools = sorted(pools) 
+ 
+    return pools[len(pools)-1]*pools[len(pools)-2]*pools[len(pools)-3]
+
 
 lines = Read_all()
 lows, ans1 = Part1(lines)
-print(lows)
+
 print("part 1: ", ans1)
-print("part 2: ", Part2(lines,lows))
+print("part 2: ",Part2(lines,lows)) #930412, 128674 rossz!
